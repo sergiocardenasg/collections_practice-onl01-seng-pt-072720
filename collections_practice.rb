99fc1df6819100db8a8f715a035dffa71560a2d6
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-01 18:51:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,4 +15,23 @@ def sort_array_char_count  (array)
 end
 
 def swap_elements (array)
+  array.sort do |a, b, c|
+    b <=> c
+  end
 end
+
+def reverse_array (array)
+end
+
+def kesha_maker (array)
+end
+
+def find_a (array)
+end
+
+def sum_array (array)
+end
+
+def add_s (array)
+end
+
